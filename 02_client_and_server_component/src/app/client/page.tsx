ba35c3d25e98d515aae68445444139981f206e38
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-07-24 00:47:06 +0530

    module 2

---
+++ 02_client_and_server_component/src/app/client/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const Client = () => {
+
+    console.log("This is a client Element")
+
+  return (
+    <div>Client</div>
+  )
+}
+
+export default Client
---
+++ 02_client_and_server_component/src/app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    
+    console.log("Server Side Component")
+
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
