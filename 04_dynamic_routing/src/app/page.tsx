9f6cfea304624643fbcac74e6ab0517166777a8d
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-07-24 01:21:55 +0530

    module 4 dynamic routing added

---
+++ 04_dynamic_routing/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const page = () => {
+  return (
+    <div>
+
+      <Link href="/dashboard/users">Link to Dashboard</Link>
+
+    </div>
+  )
+}
+
+export default page
