a614dc28482388b83906868cc51a23f198d24ba6
Sehajdeep Singh Nibber <ssingh28_be24@thapar.edu>
2026-07-24 01:07:40 +0530

    module 3 routing

---
+++ 03_routing/src/app/dashboard/users/page.tsx
@@ -0,0 +1,17 @@
+const Users = () => {
+  return (
+    <>
+    <h1> Dashboard Users</h1>
+    
+    <ul className="mt-10">
+      <li>User 1</li>
+      <li>User 2</li>
+      <li>User 3</li>
+      <li>User 4</li>
+    </ul>
+    
+    </>
+  )
+}
+
+export default Users
